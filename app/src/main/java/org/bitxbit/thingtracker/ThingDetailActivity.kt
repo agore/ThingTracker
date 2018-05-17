ef81e3c563335816a3f36fb22b2952e776978d65
Aditya <aditya.gore@gmail.com>
2018-05-16 21:01:19 -0400

    Add a FAB to add more entries to existing Thing

--- app/src/main/java/org/bitxbit/thingtracker/ThingDetailActivity.kt
+++ app/src/main/java/org/bitxbit/thingtracker/ThingDetailActivity.kt
@@ -1,7 +1,9 @@
 package org.bitxbit.thingtracker
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import io.realm.Realm
@@ -17,18 +19,25 @@ class ThingDetailActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.thing_detail_activity)
-
+        val fabAddDetail = findViewById<FloatingActionButton>(R.id.fab_add_detail)
         val recycler = findViewById<RecyclerView>(R.id.recycler_thing_details)
         recycler.layoutManager = LinearLayoutManager(this)
         realm = Realm.getDefaultInstance()
 
         val thingName = intent?.getStringExtra("thingName")
         if (thingName != null) {
-            adapter = ThingDetailRealmAdapter(this,
-                    realm.where(Thing::class.java).beginsWith("name", thingName)
-                            .findAll().sort("date", Sort.DESCENDING))
+            val results = realm.where(Thing::class.java).beginsWith("name", thingName)
+                    .findAll().sort("date", Sort.DESCENDING)
+            adapter = ThingDetailRealmAdapter(this, results)
             recycler.adapter = adapter
-        }
 
+            fabAddDetail.setOnClickListener {
+                val t = results.first()
+                val intent = Intent(this, DataEntryActivity::class.java)
+                intent.putExtra(DataEntryActivity.ITEM_NAME, t?.name)
+                intent.putExtra(DataEntryActivity.ITEM_TYPE, t?.type)
+                startActivity(intent)
+            }
+        }
     }
 }
