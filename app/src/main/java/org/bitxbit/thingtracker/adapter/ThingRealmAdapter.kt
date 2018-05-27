ac2d0cd4969b716386caa4e69051808aa49f85ec
Aditya <aditya.gore@gmail.com>
2018-05-27 18:13:53 -0400

    Use more detailed layout for showing things

--- app/src/main/java/org/bitxbit/thingtracker/adapter/ThingRealmAdapter.kt
+++ app/src/main/java/org/bitxbit/thingtracker/adapter/ThingRealmAdapter.kt
@@ -2,6 +2,8 @@ package org.bitxbit.thingtracker.adapter
 
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import io.realm.OrderedRealmCollection
@@ -9,6 +11,7 @@ import io.realm.Realm
 import io.realm.RealmRecyclerViewAdapter
 import io.realm.kotlin.createObject
 import org.bitxbit.thingtracker.MainActivity
+import org.bitxbit.thingtracker.R
 import org.bitxbit.thingtracker.ThingDetailActivity
 import org.bitxbit.thingtracker.model.Thing
 import java.text.SimpleDateFormat
@@ -21,11 +24,12 @@ class ThingRealmAdapter(act: MainActivity, data: OrderedRealmCollection<Thing>, 
     init {
         setHasStableIds(true)
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThingViewHolder = ThingViewHolder(TextView(parent.context))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThingViewHolder =
+            ThingViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.things_list, parent, false))
 
     override fun onBindViewHolder(holder: ThingViewHolder, position: Int) {
         val t: Thing? = getItem(position)
-        holder.tv.text = "${formatDate(t?.date)} [${t?.type}] ${t?.name}"
+        holder.bind(t!!)
         holder.itemView.setOnClickListener {
             val intent = Intent(activity.applicationContext, ThingDetailActivity::class.java)
             intent.putExtra("thingName", t?.name)
@@ -34,6 +38,14 @@ class ThingRealmAdapter(act: MainActivity, data: OrderedRealmCollection<Thing>, 
     }
 
     override fun getItemId(position: Int): Long = getItem(position)!!.id
+
+//    fun removeAt(adapterPosition: Int) {
+//        Realm.getDefaultInstance().executeTransaction {
+//            data?.deleteFromRealm(adapterPosition)
+//        }
+//
+//        notifyItemRemoved(adapterPosition)
+//    }
 
     companion object {
         fun createFakes(howMany: Int, realm: Realm) {
@@ -50,4 +62,12 @@ class ThingRealmAdapter(act: MainActivity, data: OrderedRealmCollection<Thing>, 
 
 }
 
-class ThingViewHolder(val tv: TextView) : RecyclerView.ViewHolder(tv)
+class ThingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val tvType : TextView = itemView.findViewById<TextView>(R.id.txt_type);
+    private val tvName : TextView = itemView.findViewById<TextView>(R.id.txt_name);
+
+    fun bind(t: Thing) {
+        tvType.text = t.typeStr
+        tvName.text = t.name
+    }
+}
