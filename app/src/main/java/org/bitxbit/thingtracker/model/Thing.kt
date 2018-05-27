0c0087fd7cd40f294a19248956b68a8d520fa3ca
Aditya <aditya.gore@gmail.com>
2018-05-27 18:14:36 -0400

    removed unused imports

--- app/src/main/java/org/bitxbit/thingtracker/model/Thing.kt
+++ app/src/main/java/org/bitxbit/thingtracker/model/Thing.kt
@@ -2,8 +2,6 @@ package org.bitxbit.thingtracker.model
 
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-import io.realm.annotations.RealmClass
-import io.realm.annotations.Required
 import java.util.*
 
 enum class ThingType {
